12452f06b422e771355a2e840b6a4510f80aa60c
BELEN <BELEN@DESKTOP-GV24251>
2025-05-21 09:22:51 +0200

    commit

---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module HolaQueTal {
+}
---
+++ src/saludo/Hola.java
@@ -0,0 +1,14 @@
+package saludo;
+
+import java.util.Scanner;
+
+public class Hola {
+	
+	public static void main (String [] args) {
+	Scanner ent=new Scanner(System.in);
+		System.out.println("Introduce un número");
+		
+		
+		
+	}
+}
